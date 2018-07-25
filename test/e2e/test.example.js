06c4ddedc1c5651f5d64a50ce8c4265a6c6bf1b5
Piotr Kowalski <piecioshka@gmail.com>
2018-07-25 14:04:16 +0200

    Update API

--- test/e2e/test.example.js
+++ test/e2e/test.example.js
@@ -3,28 +3,28 @@ const PAGE_LOAD_DELAY = 3000;
 
 module.exports = {
     'Is header contains correct text?': (browser) => {
-        browser.url(TARGET_PAGE_URL);
-        browser.waitForElementVisible('body', PAGE_LOAD_DELAY);
-        browser.assert.containsText('h2', 'Piotr Kowalski');
-        browser.end();
+        browser.url(TARGET_PAGE_URL)
+            .waitForElementVisible('body', PAGE_LOAD_DELAY)
+            .assert.containsText('h1', 'Piotr Kowalski')
+            .end();
     },
 
-    'Is main part of app contains proper number of elements?': (browser) => {
-        browser.url(TARGET_PAGE_URL);
-        browser.waitForElementVisible('body', PAGE_LOAD_DELAY);
-        browser.elements('css selector', '#menu li', (result) => {
-            if (result.value.length !== 4) {
-                browser.assert.fail('Number of elements is not correct');
-            }
-        });
-        browser.end();
+    'Is main menu contains any elements?': (browser) => {
+        browser.url(TARGET_PAGE_URL)
+            .waitForElementVisible('body', PAGE_LOAD_DELAY)
+            .elements('css selector', '#menu li', (result) => {
+                const menuSize = result.value.length;
+                browser.assert.equal(menuSize, 5);
+            })
+            .end();
     },
 
     'Capture screenshot to see manually that page looks correct': (browser) => {
-        browser.url(TARGET_PAGE_URL);
-        browser.waitForElementVisible('body', PAGE_LOAD_DELAY);
-        let filename = './screenshots/screenshot-' + new Date().toISOString() + '.png';
-        browser.saveScreenshot(filename);
-        browser.end();
+        const filename = './screenshots/screenshot-' + new Date().toISOString() + '.png';
+
+        browser.url(TARGET_PAGE_URL)
+            .waitForElementVisible('body', PAGE_LOAD_DELAY)
+            .saveScreenshot(filename)
+            .end();
     }
 };
