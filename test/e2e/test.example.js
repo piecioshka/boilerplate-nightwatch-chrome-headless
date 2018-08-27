4c1baa22b7349871ce52e6578982c6835e3666f6
Piotr Kowalski <piecioshka@gmail.com>
2018-08-27 13:19:48 +0200

    Set proper number of items in menu

--- test/e2e/test.example.js
+++ test/e2e/test.example.js
@@ -14,7 +14,7 @@ module.exports = {
             .waitForElementVisible('body', PAGE_LOAD_DELAY)
             .elements('css selector', '#menu li', (result) => {
                 const menuSize = result.value.length;
-                browser.assert.equal(menuSize, 5);
+                browser.assert.equal(menuSize, 6);
             })
             .end();
     },
