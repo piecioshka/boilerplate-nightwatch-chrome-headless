312ece78e15722807e63e9773c530b45b7403f8f
Piotr Kowalski <piecioshka@gmail.com>
2019-05-07 14:35:30 +0200

    Install missing dep: chromedriver

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -13,6 +13,12 @@ module.exports = {
     // Nie pokazujemy wszystkich szczegółów dotyczących przeprowadzania testów.
     "detailed_output": false,
 
+    "webdriver": {
+        "start_process": true,
+        "server_path": "node_modules/.bin/chromedriver",
+        "port": 9515
+    },
+
     "test_settings": {
         "default": {
             "silent": true,
